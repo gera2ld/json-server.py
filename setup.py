7827f2daf8de3d29a202da651d626fdddc17cdaa
Gerald <gera2ld@163.com>
2019-06-30 02:23:08 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gera2ld/json_server.py',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
