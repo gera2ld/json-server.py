d2abc036076269699d3233c9374fbf6ca6bbb73f
Gerald <gera2ld@live.com>
2020-11-13 15:46:44 +0800

    doc: update docs

--- README.md
+++ README.md
@@ -22,17 +22,55 @@ Or install with pip at your own risk:
 $ pip3 install json-server.py
 ```
 
+## Get Started
+
+Create a `db.json` file with following content:
+
+```json
+{
+  "posts": []
+}
+```
+
+Start a server:
+
+```sh
+$ json-server db.json
+```
+
+Create a post:
+
+```sh
+$ curl -H 'content-type: application/json' -d '{"content":"blablabla"}' http://localhost:3000/posts
+```
+
+List all posts:
+
+```sh
+$ curl http://localhost:3000/posts
+```
+
 ## Usage
 
 ```
 Usage: json-server [OPTIONS] [FILENAME]
 
+  Start a JSON server.
+
+  FILENAME refers to the JSON file where your data is stored. `db.json` will
+  be used if not specified.
+
 Options:
-  -b, --bind TEXT  the address to bind, default as `:3000`
+  --version        Show the version and exit.
+  -b, --bind TEXT  Set address to bind, default as `:3000`
   --help           Show this message and exit.
 ```
 
-## Examples
+**Note:**
+
+- Collections must be contained in your data file before starting the server, otherwise the server cannot decide the type of resources.
+
+### Examples
 
 ```sh
 # Start with default config
--- json_server/cli.py
+++ json_server/cli.py
@@ -9,18 +9,29 @@ from aiohttp.log import server_logger
 from .handlers import Handler
 from . import __version__
 
+
 @click.command()
 @click.version_option(__version__)
-@click.option('-b', '--bind', default=':3000', help='the address to bind, default as `:3000`')
+@click.option('-b',
+              '--bind',
+              default=':3000',
+              help='Set address to bind, default as `:3000`')
 @click.argument('filename', default='db.json')
 def main(bind, filename):
+    '''Start a JSON server.
+
+    FILENAME refers to the JSON file where your data is stored.
+    `db.json` will be used if not specified.
+    '''
+
     logging.basicConfig(level=logging.INFO)
-    server_logger.info(
-        'JSON Server v%s/%s %s - by Gerald',
-        __version__, platform.python_implementation(), platform.python_version())
+    server_logger.info('JSON Server v%s/%s %s - by Gerald', __version__,
+                       platform.python_implementation(),
+                       platform.python_version())
     handle = Handler(filename)
     run_forever(start_server_aiohttp(handle, bind))
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
